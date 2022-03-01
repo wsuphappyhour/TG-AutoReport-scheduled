91e55887867db3953fb65f88ea2fb42cb4490189
wsuphappyhour <petras.kovas@synergium.eu>
2022-03-01 20:11:03 +0200

    Update SetSchedule.bat

--- SetSchedule.bat
+++ SetSchedule.bat
@@ -5,8 +5,8 @@ for /f "tokens=1*" %%A in ('
   Set "MyDate=%%A"
   set "MyTime=%%B"
 )
-set "currdir=%cd%"
-SCHTASKS /Create /RU "%USERNAME%" /SC Hourly /MO 4 /TN TG-Report  /SD %MyDate% /ST %MyTime% /TR """"%currdir%\start.bat""""  
+set "currdir=%~dp0"
+SCHTASKS /Create /RU "%USERNAME%" /SC Hourly /MO 4 /TN TG-Report  /SD %MyDate% /ST %MyTime% /TR """"%currdir%\temp.bat""""  
 pause
 exit
 
